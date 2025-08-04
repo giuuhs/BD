cf7308c117deebec9b7b51e8fbff7e2bc9d2ab49
giuliaa <giuliasouzaresende0809@gmail.com>
2025-08-04 11:01:00 -0300

    Update and rename pau.sql to Nossosistema.sql

--- Nossosistema.sql
+++ Nossosistema.sql
@@ -44,38 +44,38 @@ foreign key (cod_prodid) references produto(cod_prod),
 );
 
 INSERT INTO cliente(cod_clie, nome_clie, endereco, cidade, cep, uf, cnpj, ie) VALUES
-(720, 'Ana', 'Rua 17 n.19','NiterÛi', 24358310, 'RJ', 12113231/0001-34, 2134),
-(870, 'Fl·vio', 'Av. Pres. Vargas, 10','S„o Paulo', 227631, 'SP', 22534126/9387-9, 4631),
-(110, 'Jorge', 'Rua CaiapÛ, 13', 'Curitiba', 30078500, 'PR', 145127649884-9, NULL),
-(222, 'L˙cia','Rua Itibira, 123', 'Belo Horizonte', 22124391, 'MG', 283152123/9348-8, 2985),
-(830, 'MaurÌcio', 'Av. Paluista, 1236', 'S„o Paulo', 3012638, 'SP', 32816985/7465-6, 9343),
+(720, 'Ana', 'Rua 17 n.19','Niter√≥i', 24358310, 'RJ', 12113231/0001-34, 2134),
+(870, 'Fl√°vio', 'Av. Pres. Vargas, 10','S√£o Paulo', 227631, 'SP', 22534126/9387-9, 4631),
+(110, 'Jorge', 'Rua Caiap√≥, 13', 'Curitiba', 30078500, 'PR', 145127649884-9, NULL),
+(222, 'L√∫cia','Rua Itibira, 123', 'Belo Horizonte', 22124391, 'MG', 283152123/9348-8, 2985),
+(830, 'Maur√≠cio', 'Av. Paluista, 1236', 'S√£o Paulo', 3012638, 'SP', 32816985/7465-6, 9343),
 (130, 'Edmar', 'Rua da Paria,s/n', 'Salvador', 30079300, 'BA', 23463284/234-9, 7121),
 (410, 'Rodolfo', 'Largo da Lapa,27', 'Rio de Janeiro', 30078900, 'RJ', 12835128/2346-9, 7431),
-(20, 'Beth', 'Av. ClimÈrio, 45', 'S„o Paulo', 25679300, 'SP', 32485126/324-8, 9280),
+(20, 'Beth', 'Av. Clim√©rio, 45', 'S√£o Paulo', 25679300, 'SP', 32485126/324-8, 9280),
 (157, 'Paulo', 'Trav. Moraes, casa 3', 'Londrina', null, 'PR', 3248223/324-2, 1923),
-(180, 'LÌvio', 'Av. Beira Mar, 1256','FlorianÛpolis', 30077500, 'SC', 12736571/2347-4, 1111),
-(260, 'Susana', 'Rua Lopes Mandes, 12', 'NiterÛi', 30046500, 'RJ', 21763571/232-9, 2530),
-(290, 'Renato', ' Rua Meireles, 123', 'S„o Paulo', 30225900, 'SP', 13276571/1231-4, 1820),
-(390, 'Sebasti„o', 'Rua da Igreja, 10', 'Uberaba', 30438700, 'MG', 31276547/213-3, 9071),
-(234,'JosÈ', 'Quadra 3, Bl.3, sl. 1003', 'BrasÌlia', 22841650, 'DF', 21763576/1232-3, 2931);
+(180, 'L√≠vio', 'Av. Beira Mar, 1256','Florian√≥polis', 30077500, 'SC', 12736571/2347-4, 1111),
+(260, 'Susana', 'Rua Lopes Mandes, 12', 'Niter√≥i', 30046500, 'RJ', 21763571/232-9, 2530),
+(290, 'Renato', ' Rua Meireles, 123', 'S√£o Paulo', 30225900, 'SP', 13276571/1231-4, 1820),
+(390, 'Sebasti√£o', 'Rua da Igreja, 10', 'Uberaba', 30438700, 'MG', 31276547/213-3, 9071),
+(234,'Jos√©', 'Quadra 3, Bl.3, sl. 1003', 'Bras√≠lia', 22841650, 'DF', 21763576/1232-3, 2931);
 
 INSERT INTO vendedor(cod_ven, nome_ven, salario_fixo, comissao) VALUES
-(209, 'JosÈ', 1800, 'C'),
+(209, 'Jos√©', 1800, 'C'),
 (111, 'Carlos', 2490, 'A'),
-(11, 'Jo„o', 2780, 'C'),
+(11, 'Jo√£o', 2780, 'C'),
 (240,'Antonio',9500, 'C'),
 (720, 'Felipe', 4600, 'A'),
 (213, 'Jonas', 2300, 'A'),
-(101, 'Jo„o', 2650, 'C'),
+(101, 'Jo√£o', 2650, 'C'),
 (310, 'Josias', 870, 'B'),
-(250, 'MaurÌcio', 2930, 'B');
+(250, 'Maur√≠cio', 2930, 'B');
 
 insert into produto (cod_prod, unidade, descricao, val_unit) values
 (25, 'KG', 'Queijo', 0.97),
 (31, 'Bar', 'Choicolate',0.87),
 (78, 'L', 'Vinho', 2.00),
 (22, 'M', 'Linho', 0.11),
-(11, 'SAC', 'AÁ˙car', 0.30),
+(11, 'SAC', 'A√ß√∫car', 0.30),
 (53, 'M', 'Linha', 1.80),
 (13, 'G', 'Ouro', 6.18),
 (45, 'M', 'Madeira', 0.25),
@@ -140,3 +140,29 @@ SELECT * FROM pedido;
 
 -- 6 --
 SELECT * FROM pedido ORDER BY cod_venid;
+
+--Exerc√≠cios: Base Nosso Sistema
+
+--Criar um relat√≥rio que mostre o nome do vendedor e seus pedidos em ordem de pedido de forma crescente.
+
+--Criar um relat√≥rio que mostre o n√∫mero do pedido, nome do vendedor e nome do cliente.
+
+--Mostre o nome do vendedor e o nome dos clientes que ele j√° atendeu.
+
+--Mostre o pedido (n√∫mero) e a descri√ß√£o dos produtos que ele tem.
+
+--Crie um relat√≥rio que mostre os produtos comprados por cada cliente.
+
+--Crie um relat√≥rio que mostre os produtos vendidos por cada vendedor.
+
+--Qual cliente comprou chocolate?
+
+--Qual vendedor vendeu mais chocolate?
+
+-- INNER JOIN
+SELECT nome_ven, num_pedido from vendedor INNER JOIN pedido ON cod_ven = cod_venid;
+
+SELECT nome_ven, num_pedido, nome_clie from vendedor, cliente INNER JOIN pedido ON cod_venid = cod_venid and cod_clie = cod_clieid;
+
+SELECT v.nome_ven, c.nome_clie from pedido as p INNER JOIN vendedor as v ON v.cod_ven = p.cod_venid INNER JOIN cliente as c ON c.cod_clie = p.cod_clieid;
+
